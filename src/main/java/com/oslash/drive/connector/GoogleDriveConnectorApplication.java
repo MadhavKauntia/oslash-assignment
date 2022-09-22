b0211345ac9af73b920a627619c475783a1359b5
Madhav Kauntia <mkauntia@gmail.com>
2022-09-22 14:19:05 +0530

    chore: initial commit to set up repository

---
+++ src/main/java/com/oslash/drive/connector/GoogleDriveConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.oslash.drive.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleDriveConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oslash/drive/connector/GoogleDriveConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslash.drive.connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDriveConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
