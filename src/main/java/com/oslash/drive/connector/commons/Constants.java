248e6d1d6b9608da62e6d055fce169fc56f9182c
Madhav Kauntia <mkauntia@gmail.com>
2022-09-24 14:49:02 +0530

    fix: change background thread name so that it fits within log

--- src/main/java/com/oslash/drive/connector/commons/Constants.java
+++ src/main/java/com/oslash/drive/connector/commons/Constants.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 public class Constants {
     public static final String GOOGLE_DRIVE_BASE_URI = "https://www.googleapis.com";
     public static final String HEADER_ACCEPT = "Accept";
-    public static final String BACKGROUND_THREAD_PREFIX = "backgroundThread-";
+    public static final String BACKGROUND_THREAD_PREFIX = "bgThread-";
     public static final String SERVICE_TASK_EXECUTOR = "serviceTaskExecutor";
 }
